0a1e003c1f7dcbee4723d3bebdc0bb1fed4e8356
Phil25 <Phil25@ProtonMail.com>
2019-08-04 15:13:04 +0200

    Groundwork for pytest integration tests

---
+++ test/integration/common.py
@@ -0,0 +1,46 @@
+import subprocess
+import os
+import shutil
+
+BINARY = './bin/smam'
+
+class SMAM:
+
+    def __init__(self, binary = BINARY, destination = './mod/'):
+        self.binary = binary
+        self.destination = destination
+        self.smroot = destination + 'addons/sourcemod/'
+
+    def __enter__(self):
+        os.makedirs(self.smroot + 'configs/')
+        os.makedirs(self.smroot + 'data/')
+        os.makedirs(self.smroot + 'gamedata/')
+        os.makedirs(self.smroot + 'plugins/')
+        os.makedirs(self.smroot + 'scripting/')
+        os.makedirs(self.smroot + 'translations/')
+
+        columns = shutil.get_terminal_size().columns
+        print('\n\033[95m\033[1m' + ''.center(columns, '=') + '\033[0m')
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        shutil.rmtree(self.destination)
+
+    def exec(self, cmd):
+        cmds = [self.binary] + cmd.split() + ['-d', self.destination]
+        columns = shutil.get_terminal_size().columns
+        print('\n\033[95m\033[1m' + ' '.join(cmds).center(columns, '-') + '\033[0m')
+        return subprocess.run(cmds)
+
+    def exists(self, f):
+        return os.path.exists(self.smroot + f)
+
+    def isfile(self, f):
+        return os.path.isfile(self.smroot + f)
+
+    def isdir(self, f):
+        return os.path.isdir(self.smroot + f)
+
+    def getsize(self, f):
+        return os.path.getsize(self.smroot + f)
---
+++ test/integration/test_install.py
@@ -0,0 +1,56 @@
+from common import SMAM
+from os import path
+import pytest
+
+INSTALL_DATA = {
+    'advancedinfiniteammo': [
+        ['plugins/AdvancedInfiniteAmmo.smx', 23290]
+    ],
+    'afk_manager': [
+        ['plugins/afk_manager4.smx', 24919],
+        ['translations/afk_manager.phrases.txt', 14663]
+    ],
+    'dynamic_motd': [
+        ['plugins/dynamic_motd.smx', 16171]
+    ],
+    'funcommandsx': [
+        ['plugins/funcommandsX.smx', 38290],
+        ['configs/happy_phrases.ini', 364],
+        ['translations/funcommandsX.phrases.txt', 2618],
+        ['translations/ru/funcommandsX.phrases.txt', 3392],
+        ['translations/tr/funcommandsX.phrases.txt', 3000]
+    ],
+    'tf2attributes': [
+        ['plugins/tf2attributes.smx', 12701],
+        ['gamedata/tf2.attributes.txt', 3287]
+    ],
+    'tf2items': [
+        ['scripting/tf2items_manager.sp', 15646],
+        ['scripting/include/tf2items.inc', 9463],
+        ['extensions/tf2items.ext.2.ep2v.so', 172477]
+    ],
+    'thriller': [
+        ['plugins/thriller.smx', 3691],
+        ['gamedata/thriller.plugin.txt', 656]
+    ],
+    'rtd': [
+        ['configs/rtd2_perks.default.cfg', 30829],
+        ['plugins/rtd.smx', 124057],
+        ['scripting/rtd.sp', 51622],
+        ['translations/rtd2.phrases.txt', 11692],
+        ['translations/rtd2_perks.phrases.txt', 15264]
+    ],
+    'updater': [
+        ['plugins/updater.smx', 22011]
+    ]
+}
+
+@pytest.mark.parametrize('addon', INSTALL_DATA)
+def test_install(addon):
+    with SMAM() as smam:
+        smam.exec('install ' + addon)
+
+        for (filename, size) in INSTALL_DATA[addon]:
+            assert smam.exists(filename)
+            assert smam.isfile(filename)
+            assert size == smam.getsize(filename)
